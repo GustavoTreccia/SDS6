830e27c0e70be32045b4dfaec30b36b89f2efba6
GustavoTreccia <g.treccia@outlook.com>
2022-01-10 22:18:19 -0300

    Primeiro commit

---
+++ backend/src/test/java/com/backend/devsuperior/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
