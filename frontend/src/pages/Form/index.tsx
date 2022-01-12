475f9afc70496b6a35ecf5893b7bec03dfa5ff87
GustavoTreccia <g.treccia@outlook.com>
2022-01-11 22:19:29 -0300

    Rotas adicionadas

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form() {
+    return(
+        <h1>Boa noite</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return(
+        <h1>Boa tarde</h1>
+    );
+
+}
+export default Listing;
