e93d532214a8580ae189612f5dddd6b0b2f61fcb
GustavoTreccia <g.treccia@outlook.com>
2022-01-15 22:59:31 -0300

    useState useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,22 @@
 
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
+            const data = response.data as MoviePage;
+                setPageNumber(data.number);
+        });
+    }, []);
+
     return (
         <>
             <Pagination />
@@ -30,8 +44,6 @@ export default Listing;
 
 /* FORMA ERRADA
 
-    axios.get(`${BASE_URL}`).then(response => {
-        console.log(response.data);
-    });
+    
 
 */
